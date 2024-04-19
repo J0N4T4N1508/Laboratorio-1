0724666f647ac0698b30cd605b24bc74f5f5c14b
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-04-19 11:58:02 -0500

    laboratorio2

---
+++ CursoEstructuraDeDatos2024/Laboratorio 2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+
+var n = 10;
+var suma = 0;
+
+for (var i = 0; i <= n; i++) 
+{ 
+    suma = suma + i;
+}
+
+
+
+Console.WriteLine("El resultado es: " + suma.ToString());
+
